9a1d39dcde847c57ee885fcddcc09f92b9d6a9a8
AlexeyKonstantinov78 <alex_kav@mail.ru>
2022-08-29 14:51:44 +0300

    Ffmpeg executor

---
+++ src/commands/ffmpeg/ffmpeg.executor.ts
@@ -0,0 +1,46 @@
+import { ChildProcessWithoutNullStreams, spawn } from "child_process";
+import { CommandExecuter } from "../../core/executor/command.executor";
+import { FileService } from "../../core/files/files.services";
+import { IStreamLogger } from "../../core/handlers/stream-logger.interface";
+import { StreamHandler } from "../../core/handlers/stream.handler";
+import { PromptService } from "../../core/prompt/prompt.service";
+import { FfmpegBuilder } from "./ffmpeg.builder";
+import { ICommandExecFfmpeg, IFfmpegInput } from "./ffmpeg.types";
+
+export class FfmpegExecutor extends CommandExecuter<IFfmpegInput> {
+  private fileService: FileService = new FileService();
+  private promptService: PromptService = new PromptService();
+
+  constructor(logger: IStreamLogger) {
+    super(logger);
+  }
+
+
+
+  protected async prompt(): Promise<IFfmpegInput> {
+    const width = await this.promptService.input<number>('Ширина', 'number');
+    const height = await this.promptService.input<number>('Высота', 'number');
+    const path = await this.promptService.input<string>('путь до файла', 'input');
+    const name = await this.promptService.input<string>('имя', 'input');
+    return { width, height, name, path };
+  }
+  
+  protected build({ width, height, name, path }: IFfmpegInput): ICommandExecFfmpeg {
+    const output = this.fileService.getFilePath(path, name, 'mp4');
+    const args = (new FfmpegBuilder)
+      .input(path)
+      .setVideoSize(width, height)
+      .output(output);
+
+    return { command: 'ffmpeg', args, output };
+  }
+  protected spawn({output, command, args}:  ICommandExecFfmpeg): ChildProcessWithoutNullStreams {
+    this.fileService.deleteFileIfExists(output);
+    return spawn(command, args);
+  }
+  protected processStream(stream: ChildProcessWithoutNullStreams, logger: IStreamLogger): void {
+    const handler = new StreamHandler(logger);
+    handler.proccessOutput(stream);
+  }
+  
+}
